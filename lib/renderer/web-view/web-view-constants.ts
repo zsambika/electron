c34bf73c2e6be976462bfd5927456bc640645be3
Milan Burda <milan.burda@gmail.com>
2022-08-11 18:28:37 +0200

    refactor: separate WEB_VIEW_ATTRIBUTES

--- lib/renderer/web-view/web-view-constants.ts
+++ lib/renderer/web-view/web-view-constants.ts
@@ -1,20 +1,21 @@
-export const enum WEB_VIEW_CONSTANTS {
-  // Attributes.
-  ATTRIBUTE_NAME = 'name',
-  ATTRIBUTE_PARTITION = 'partition',
-  ATTRIBUTE_SRC = 'src',
-  ATTRIBUTE_HTTPREFERRER = 'httpreferrer',
-  ATTRIBUTE_NODEINTEGRATION = 'nodeintegration',
-  ATTRIBUTE_NODEINTEGRATIONINSUBFRAMES = 'nodeintegrationinsubframes',
-  ATTRIBUTE_PLUGINS = 'plugins',
-  ATTRIBUTE_DISABLEWEBSECURITY = 'disablewebsecurity',
-  ATTRIBUTE_ALLOWPOPUPS = 'allowpopups',
-  ATTRIBUTE_PRELOAD = 'preload',
-  ATTRIBUTE_USERAGENT = 'useragent',
-  ATTRIBUTE_BLINKFEATURES = 'blinkfeatures',
-  ATTRIBUTE_DISABLEBLINKFEATURES = 'disableblinkfeatures',
-  ATTRIBUTE_WEBPREFERENCES = 'webpreferences',
+export const enum WEB_VIEW_ATTRIBUTES {
+  NAME = 'name',
+  PARTITION = 'partition',
+  SRC = 'src',
+  HTTPREFERRER = 'httpreferrer',
+  NODEINTEGRATION = 'nodeintegration',
+  NODEINTEGRATIONINSUBFRAMES = 'nodeintegrationinsubframes',
+  PLUGINS = 'plugins',
+  DISABLEWEBSECURITY = 'disablewebsecurity',
+  ALLOWPOPUPS = 'allowpopups',
+  PRELOAD = 'preload',
+  USERAGENT = 'useragent',
+  BLINKFEATURES = 'blinkfeatures',
+  DISABLEBLINKFEATURES = 'disableblinkfeatures',
+  WEBPREFERENCES = 'webpreferences',
+}
 
+export const enum WEB_VIEW_CONSTANTS {
   // Error messages.
   ERROR_MSG_ALREADY_NAVIGATED = 'The object has already navigated, so its partition cannot be changed.',
   ERROR_MSG_INVALID_PARTITION_ATTRIBUTE = 'Invalid partition attribute.',
